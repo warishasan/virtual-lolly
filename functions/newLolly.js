050d48e2ccb71a8ff6fe59072d1123349d556118
warishasan <waris.hasan00@gmail.com>
2020-10-29 23:27:09 +0500

    added deploy url to gatsby source graphql plugin

--- functions/newLolly.js
+++ functions/newLolly.js
@@ -86,6 +86,16 @@ const resolvers = {
           data: args
         })
       )
+
+      axios.post('https://api.netlify.com/build_hooks/5f9b08201c44a833a923d4b4')
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+
+
       console.log(result)
       return result.data
     },
